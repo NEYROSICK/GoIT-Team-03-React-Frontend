964f96bcb83300300148bb75d32617597ae034f5
NEYROSICK <dhatak24@gmail.com>
2023-11-17 17:23:35 +0200

    First Setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,11 @@
-import {
-  HeaderContainer,
-  Navigation,
-  StyledLink,
-  IconWrapper,
-} from './Header.styled';
-import sprite from '../../assets/sprite.svg';
+import { Link } from 'react-router-dom';
+import { HeaderContainer, Navigation } from './Header.styled';
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <Navigation>
-        <StyledLink to="/first">
-          <IconWrapper>
-            <use href={`${sprite}#icon-logo`} />
-          </IconWrapper>
-          First
-        </StyledLink>
-        <StyledLink to="/second">
-          <IconWrapper>
-            <use href={`${sprite}#icon-logo`} />
-          </IconWrapper>
-          Second
-        </StyledLink>
+        <Link to="/hello">Header</Link>
       </Navigation>
     </HeaderContainer>
   );
---
+++ src/pages/HelloPage/HelloPage.jsx
@@ -0,0 +1,11 @@
+import { Container } from './HelloPage.styled';
+
+const HelloPage = () => {
+  return (
+    <Container>
+      <h1>Outlet</h1>
+    </Container>
+  );
+};
+
+export default HelloPage;
