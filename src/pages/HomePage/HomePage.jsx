d0371dae73223e16fdc16c66bc4702ec8a11baac
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-11-20 22:00:10 +0200

    home_page_backgroundImage+mainImage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
-import { Container } from './HomePage.styled';
-import UserPage from '../UserPage/UserPage';
+import { Container, ImageDiv } from './HomePage.styled';
+import image from '../../images/main-page.png'
+// import UserPage from '../UserPage/UserPage';
 const HomePage = () => {
   return (
     <Container>
-      <h1>Outlet</h1>
-      <UserPage />
+      {/* <h1>Outlet</h1>
+      <UserPage /> */}
+      <ImageDiv>
+        <h1>Take good care of your small pets</h1>
+        <img src={image} />
+      </ImageDiv>
     </Container>
   );
 };
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -3,7 +3,12 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction:column;
+  flex-direction: column;
   align-items: center;
   padding-top: 500px;
+`;
+
+export const ImageDiv = styled.div`
+  padding-top: 0px;
+  display: flex;
 `;
