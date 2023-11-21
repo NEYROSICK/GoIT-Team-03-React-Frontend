03e0846dff9672be9be464f8e4a89d04c6e8d219
VanyaBizhko <vanyabigko@gmail.com>
2023-11-21 19:46:39 +0200

    додали логінізацію і реєстрацію
    Co-authored-by: Ivan-T0 <Ivan-T0@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import HomePage from './pages/HomePage/HomePage';
 import AddPetPage from './pages/AddPetPage/AddPetPage';
 import NoticesPage from './pages/NoticesPage/NoticesPage';
 import UserPage from './pages/UserPage/UserPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/register" element={<RegisterPage/>}/>
           <Route index element={<HomePage />} />
           <Route path="main" element={<Navigate to="/" />} />
           <Route path="addPet" element={<AddPetPage />} />
---
+++ src/redux/auth/operations.jsx
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://goit-team-03-node.onrender.com/api/';
+
+const setAuthHeader = token =>
+  (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+
+const clearAuthHeader = () =>
+  (axios.defaults.headers.common.Authorization = '');
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/auth/register', credentials);
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/auth/login', credentials);
+        setAuthHeader(response.data.token);
+        console.log("ecgsiyj");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/auth/logout');
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
