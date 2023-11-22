ed0415f3aa43daaae707913f04b7666b5a1c4538
NEYROSICK <dhatak24@gmail.com>
2023-11-22 07:23:13 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { store } from './redux/store';
 import './index.css';
+import './fonts/manrope-v15-latin-regular.woff2';
+import './fonts/manrope-v15-latin-500.woff2';
+import './fonts/manrope-v15-latin-600.woff2';
+import './fonts/manrope-v15-latin-700.woff2';
+import './fonts/manrope-v15-latin-800.woff2';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
