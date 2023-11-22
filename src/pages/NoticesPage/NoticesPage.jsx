f897184c4fae16d58903b08124e34f4ed6aeacbc
Z1NG0D <YaroslavBorovskiy@gmail.com>
2023-11-22 22:23:54 +0100

    comit
    made with unity and andy doUrden

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,50 +1,82 @@
-import NoticesSearch from "../../components/NoticesSearch/NoticesSearch"
-import NoticesCategoriesNav from "../../components/NoticesCategoriesNav/NoticesCategoriesNav";
-import NoticesFilter from "../../components/NoticesFilter/NoticesFilter"
+import NoticesSearch from '../../components/NoticesSearch/NoticesSearch';
+import NoticesCategoriesNav from '../../components/NoticesCategoriesNav/NoticesCategoriesNav';
+import NoticesFilter from '../../components/NoticesFilter/NoticesFilter';
 import { useSearchParams } from 'react-router-dom';
 import { useEffect, useState, useCallback } from 'react';
-import { Container, FilterAndAddContainer, FilterContainer, PageTitle } from "../NoticesPage/NoticesPage.styled";
-import AddPetButton from "../../components/AddPetButton/AddPetButton";
-
+import {
+  Container,
+  FilterAndAddContainer,
+  FilterContainer,
+  PageTitle,
+} from '../NoticesPage/NoticesPage.styled';
+import AddPetButton from '../../components/AddPetButton/AddPetButton';
+import {
+  useGetNoticesQuery,
+  useGetOneNoticeQuery,
+  useGetMyNoticesQuery,
+} from '../../redux/API/noticesApi';
 const NoticesPage = () => {
+  const {
+    data: allNotices,
+    error: allNoticesError,
+    isLoading: loadAllNotices,
+  } = useGetNoticesQuery({
+    // your params
+  });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const {
+    data: myNotices,
+    error: myNoticesError,
+    isLoading: loadMyNotices,
+  } = useGetMyNoticesQuery({
+    // your params
+  });
+  const {
+    data: oneNotice,
+    error: oneNoticeError,
+    isLoading: loadOneNotice,
+  } = useGetOneNoticeQuery({
+    id: 'your-notice-id',
+    params: {
+      /* your params */
+    },
+  });
 
-    const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
 
-    const query = searchParams.get('query');
+  const handleSubmit = ({ query }) => {
+    searchParams.set('query', query);
+    setSearchParams(searchParams);
+    resetPage();
+  };
 
-    const handleSubmit = ({ query }) => {
-        searchParams.set('query', query);
-        setSearchParams(searchParams);
-        resetPage();
-    };
+  const handleClear = () => {
+    searchParams.delete('query', query);
+    setSearchParams(searchParams);
+    resetPage();
+  };
 
-    const handleClear = () => {
-        searchParams.delete('query', query);
-        setSearchParams(searchParams);
-        resetPage();
-    };
+  const resetPage = useCallback(() => {
+    setSearchParams(searchParams);
+  }, [searchParams, setSearchParams]);
 
-    const resetPage = useCallback(() => {
-        setSearchParams(searchParams);
-    }, [searchParams, setSearchParams]);
+  return (
+    <Container>
+      <PageTitle>Find your favorite pet</PageTitle>
+      <div>
+        <NoticesSearch onSubmit={handleSubmit} onClear={handleClear} />
+      </div>
+      <FilterContainer>
+        <NoticesCategoriesNav searchParams={searchParams} />
+        <div>
+          <FilterAndAddContainer>
+            <NoticesFilter />
+            <AddPetButton />
+          </FilterAndAddContainer>
+        </div>
+      </FilterContainer>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <PageTitle>Find your favorite pet</PageTitle>
-            <div>
-                <NoticesSearch onSubmit={handleSubmit} onClear={handleClear}/>
-            </div>
-            <FilterContainer>
-                <NoticesCategoriesNav searchParams={searchParams}/>
-                <div>
-                    <FilterAndAddContainer>
-                        <NoticesFilter/>
-                        <AddPetButton/>
-                    </FilterAndAddContainer>
-                </div>
-            </FilterContainer>
-        </Container>
-    )
-}
-
-export default NoticesPage
+export default NoticesPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/authSlice';
 import { UserApi } from './API/UserApi';
+import {noticesApi} from './API/noticesApi'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   [UserApi.reducerPath]: UserApi.reducer,
+  [noticesApi.reducerPath]: noticesApi.reducer,
 });
 
 export const store = configureStore({
