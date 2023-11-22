65ddeab3545564523525621e9940203a709c3fcb
NEYROSICK <dhatak24@gmail.com>
2023-11-22 06:50:34 +0200

    Update AddPetButton.jsx

--- src/components/AddPetButton/AddPetButton.jsx
+++ src/components/AddPetButton/AddPetButton.jsx
@@ -1,17 +1,21 @@
-import { AddButton, StyledPlusIcon, StyledPlusIconSmall, AddButtonText } from './AddPetButton.styled';
+import {
+  AddButton,
+  StyledPlusIcon,
+  StyledPlusIconSmall,
+  AddButtonText,
+} from './AddPetButton.styled';
 
 const AddPetButton = () => {
-
-    const handleClick = () => {
-        console.log('Sign in to add your own notice.')
-    };
-    return (
-        <AddButton onClick={handleClick} to={'/addPet'}>
-            <StyledPlusIcon/>
-            <AddButtonText>Add Pet</AddButtonText>
-            <StyledPlusIconSmall/>
-        </AddButton>
-    )
-}
+  const handleClick = () => {
+    console.log('Sign in to add your own notice.');
+  };
+  return (
+    <AddButton onClick={handleClick} to={'/add-pet'}>
+      <StyledPlusIcon />
+      <AddButtonText>Add Pet</AddButtonText>
+      <StyledPlusIconSmall />
+    </AddButton>
+  );
+};
 
 export default AddPetButton;
