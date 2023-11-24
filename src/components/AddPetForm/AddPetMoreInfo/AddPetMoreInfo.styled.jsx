e67f5228055a1e56cfecaa723bd7f033d24a8811
NEYROSICK <dhatak24@gmail.com>
2023-11-24 17:43:06 +0200

    Update AddPetMoreInfo.styled.jsx

--- src/components/AddPetForm/AddPetMoreInfo/AddPetMoreInfo.styled.jsx
+++ src/components/AddPetForm/AddPetMoreInfo/AddPetMoreInfo.styled.jsx
@@ -1,26 +1,29 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 // import {Field} from "formik";
 
 export const PhotoContainer = styled.img`
-    background: var(--clr-brand-blue-lt);
-    display: block;
-    width: 182px;
-    height: 182px;
-    border-radius: 40px;
+  background: var(--clr-brand-blue-lt);
+  display: block;
+  width: 182px;
+  height: 182px;
+  border-radius: 40px;
 `;
 
 export const LabelInputFile = styled.label`
-    background: var(--clr-brand-blue-lt);
-    display: block;
-    width: 182px;
-    height: 182px;
-    border-radius: 40px;
+  background: var(--clr-brand-blue-lt);
+  display: block;
+  width: 182px;
+  height: 182px;
+  border-radius: 40px;
 
-    &.no-image-selected {
-        border: 2px solid red;
-    }
+  &.no-image-selected {
+    border: 2px solid red;
+  }
 `;
 
-export const InputFile = styled.input`
-    display: none;
+export const InputFile = styled.input.attrs({
+  type: 'file',
+  accept: 'image/png, image/jpeg',
+})`
+  display: none;
 `;
